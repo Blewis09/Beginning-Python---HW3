6998bc0781bb05f2dac3d0809881b9fd5d7f3a87
Bethany Lewis <bethanylewis09@gmail.com>
2023-12-30 19:11:42 -0500

    first commit

---
+++ modulehw_circle.py
@@ -0,0 +1,6 @@
+#Circle circumference import
+#circumference = 2 * pi * radius
+def circle_circumference():
+    x = int(input("What is the radius of your circle? "))
+    pi = 3.14
+    return (2 * pi * x)
---
+++ modulehw_squarefootage.py
@@ -0,0 +1,6 @@
+#SquareFootage import
+#Length X Width == Area
+def square_footage():
+    x = int(input("What is the length of your house? "))
+    y = int(input("What is the width of your house? "))
+    return (x*y)
